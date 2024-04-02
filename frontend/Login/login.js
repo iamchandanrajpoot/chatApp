f75e0c820e40e8a64faa4610344aa0f72392d4a9
iamchandanrajpoot <iamchandanrajpoot@gmail.com>
2024-04-02 18:46:28 +0530

    create frontend for login page

---
+++ frontend/Login/login.js
@@ -0,0 +1,31 @@
+const loginForm = document.getElementById("login-form");
+
+loginForm.onsubmit = async (e) => {
+  try {
+    e.preventDefault();
+    const userData = {
+      email: e.target.email.value,
+      password: e.target.password.value,
+    };
+    console.log(userData);
+    const response = await fetch("http://localhost:3000/user/login", {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(userData),
+    });
+    console.log(response);
+    const userResult = await response.json();
+    if (userResult.success) {
+      e.target.email.value = "";
+      e.target.password.value = "";
+      alert(userResult.message);
+    } else {
+      alert(userResult.message);
+    }
+    console.log(userResult);
+  } catch (error) {
+    console.log(error);
+  }
+};
