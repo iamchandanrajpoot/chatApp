39e3af341d7459c61eb492d59d76a546fcb091f6
iamchandanrajpoot <iamchandanrajpoot@gmail.com>
2024-04-02 19:20:02 +0530

    update login backend api

--- controllers/login.js
+++ controllers/login.js
@@ -6,22 +6,29 @@ exports.loginController = async (req, res) => {
   try {
     console.log(req.body);
     const user = await User.findOne({ where: { email: req.body.email } });
-    if (user && (await bcrypt.compare(req.body.password, user.password))) {
-      jwt.sign({ id: user.id }, "privateKey", (err, token) => {
-        if (err) {
-          return res
-            .status(500)
-            .json({ message: "internal server error", success: false });
-        } else {
-          return res
-            .status(200)
-            .json({ message: "successfully login", token, success: true });
+    if (user) {
+        if((await bcrypt.compare(req.body.password, user.password))){
+            jwt.sign({ id: user.id }, "privateKey", (err, token) => {
+                if (err) {
+                  return res
+                    .status(500)
+                    .json({ message: "internal server error", success: false });
+                } else {
+                  return res
+                    .status(200)
+                    .json({ message: "successfully login", token, success: true });
+                }
+            });
+        }else{
+            res
+            .status(401)
+            .json({ message: "User not authorized", success: false });
         }
-      });
+      
     } else {
       res
         .status(404)
-        .json({ message: "wrong email or password", success: false });
+        .json({ message: "User not found", success: false });
     }
   } catch (error) {
     console.error(error);
